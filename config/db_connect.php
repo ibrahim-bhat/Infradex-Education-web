4c97512ae140f986dd11a489c9917a37cfbebd95
IBRAHIM BHAT <101333662+ibrahim-bhat@users.noreply.github.com>
2025-02-20 01:30:02 +0530

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,9 +1,12 @@
 <?php
 $servername = "localhost";
-$username = "root";  // Replace with your database username
-$password = "";  // Replace with your database password
-$dbname = "phpinfradex";
+// $username = "root";  // Replace with your database username
+// $password = "";  // Replace with your database password
+// $dbname = "phpinfradex";
 
+$username = "u873877420_infradexedu";  // Replace with your database username
+$password = "infradexedu123@";  // Replace with your database password
+$dbname = "u873877420_infradexedu";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
