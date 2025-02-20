02276b8b8a9b5e5c36bcf918e393cf6ac6809e03
IBRAHIM BHAT <101333662+ibrahim-bhat@users.noreply.github.com>
2025-02-20 15:12:07 +0530

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,18 +1,15 @@
 <?php
 $servername = "localhost";
-// $username = "root";  // Replace with your database username
-// $password = "";  // Replace with your database password
-// $dbname = "phpinfradex";
-
-$username = "u873877420_infradexedu";  // Replace with your database username
-$password = "Infradexedu123@";  // Replace with your database password
+$username = "u873877420_infradexedu";  
+$password = "Infradexedu123@";  
 $dbname = "u873877420_infradexedu";
-// Create connection
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
 
-?> 
+try {
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        throw new Exception("Connection failed: " . $conn->connect_error);
+    }
+} catch (Exception $e) {
+    die($e->getMessage());
+}
+?>
