0f8364d20c3c6f1770ebbd8666276d0b8902d14b
IBRAHIM BHAT <101333662+ibrahim-bhat@users.noreply.github.com>
2025-02-20 08:12:58 +0530

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -12,6 +12,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Server Error ");
 }
 ?> 
